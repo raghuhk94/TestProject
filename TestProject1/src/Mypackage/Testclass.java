665737f2f89f9679b1bca41e4a91b1f438f5417f
Raghavendra <Raghavendra@DESKTOP-F838K3J>
2022-07-21 16:49:50 +0530

    Added one more SOP last time

--- TestProject1/src/Mypackage/Testclass.java
+++ TestProject1/src/Mypackage/Testclass.java
@@ -8,6 +8,7 @@ public class Testclass {
 		System.out.println("New Code Added but changed");
 		System.out.println("Added One more new Code");
 		System.out.println("Added SOP Statement");
+		System.out.println("May be last SOP");
 	}
 
 }
